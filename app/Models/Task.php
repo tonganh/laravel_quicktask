8b9011eda0905e1b0ea3bcd7aee163f29f5f2872
anhtn <tongngocanhcampha@gmail.com>
2022-08-16 14:10:04 +0700

    Create relations for current database

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -38,4 +38,9 @@ class Task extends Model
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
